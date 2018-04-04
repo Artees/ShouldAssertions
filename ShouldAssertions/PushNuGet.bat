878fafc4f5a80dc12787ea4c02bc40f4afb53608
Artees <Artees@users.noreply.github.com>
2018-04-04 21:06:46 +0300

    Updated .nuspec and README.md

--- ShouldAssertions/PushNuGet.bat
+++ ShouldAssertions/PushNuGet.bat
@@ -1,3 +1,3 @@
 cd /D "%~dp0"
 cd ..\..\..\LocalNuGet
-nuget push ShouldAssertions.1.0.0.nupkg -Source https://api.nuget.org/v3/index.json
+nuget push ShouldAssertions.1.0.1.nupkg -Source https://api.nuget.org/v3/index.json
